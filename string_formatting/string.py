dc930627858b80440d48e1569d8d8efb35500c9e
anish2000afk <anishkandulna@gmail.com>
2023-07-12 10:39:30 +0530

    new changes

---
+++ string_formatting/string.py
@@ -0,0 +1,7 @@
+dict = {1: "anish", 2: "foden ", 3: "kyle"}
+x = 1
+for i in dict.items():
+    if i == x :
+        print("found")
+    else:
+        print('nf')
--- string_formatting/string_format.py
+++
@@ -1,4 +0,0 @@
-def iterator(n):
-    for i in range(1,n+1):
-        print(i)
-
