25c4710b6e397a472107babb1aa084c673ce75d9
Anish <anishkandulna@gmail.com>
2024-04-21 16:54:20 +0530

    new changes just for testing

--- decimal_to_binary_converter.py
+++ decimal_to_binary_converter.py
@@ -1,15 +1,14 @@
 n = int(input())
 i = 2
 list = []
-if (n%2 == 0):
+if n % 2 == 0:
     list.append(0)
 else:
     list.append(1)
-while (n//i >= 1):
-        remainder = (n//i)%2 # The zero-division error
-        list.append(remainder)
-        i = i*2
+while n // i >= 1:
+    remainder = (n // i) % 2  # The zero-division error
+    list.append(remainder)
+    i = i * 2
 list = list[::-1]
 print("".join(str(list)))
 # Next task to create a join
-
